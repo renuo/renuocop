ebbf6c706278f12888d056a1f4e7ca118e3e1f04
Raphael Nestler <raphael.nestler@renuo.ch>
2025-03-12 12:03:21 +0100

    Follow our own styleguide (#19)
    * Add .rubocop.yml
    
    * Apply rubocop -a fixes
    
    * Check style in GitHub Actions

--- test/test_renuocop.rb
+++ test/test_renuocop.rb
@@ -4,7 +4,7 @@ require "test_helper"
 
 class TestRenuocop < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::Renuocop::VERSION
+    assert_not_nil ::Renuocop::VERSION
   end
 
   def test_it_does_something_useful
