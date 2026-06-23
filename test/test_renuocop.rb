1ff6fb76f648acf87fb11092c0247d9fe555e93e
Josua Schmid <josua.schmid@renuo.ch>
2026-06-23 15:00:20 +0200

    Fix failing bin/check

--- test/test_renuocop.rb
+++ test/test_renuocop.rb
@@ -4,10 +4,6 @@ require "test_helper"
 
 class TestRenuocop < Minitest::Test
   def test_that_it_has_a_version_number
-    assert_not_nil ::Renuocop::VERSION
-  end
-
-  def test_it_does_something_useful
-    assert false
+    assert ::Renuocop::VERSION
   end
 end
